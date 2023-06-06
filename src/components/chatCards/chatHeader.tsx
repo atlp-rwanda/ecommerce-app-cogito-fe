a0bb1cb509b5bc03325fd73fb541155c3fc442ba
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-06-06 08:09:04 +0200

    chore(setting-project): Setting up an empty react project (#1)
    -Create react app project with vite
    -configure tslint.json
    
    [Finish #16]

---
+++ src/components/chatCards/chatHeader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChatHeader: React.FC = () => {
+  return (
+    <div className="chat-card">
+      <div className="bg-gradient-to-r from-blue-500 to-purple-500 py-4">
+        <h1 className="text-white text-2xl font-bold text-center">Chat App - E-Cogito.com</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
