22947cb3b9179125d2af7998cec3333af10eabb3
Illustre13 <ndahayosibertin17@gmail.com>
2023-06-06 08:09:04 +0200

    chore(setting-redux-tool-kit): Setting up our app to work with Redux Tool Kit - created the redux store, the reducer, and the action to handle all possible state - created component to interact with the redux store - rendered the component to our react app using  library - configured dotenv variables to be used with axios while calling our api url
    [Finish #15]

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import IndexStore from "./redux/store/index";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+
   <React.StrictMode>
-    <App />
+    <Provider store={IndexStore}>
+       <App />
+        </Provider>
   </React.StrictMode>
 );
