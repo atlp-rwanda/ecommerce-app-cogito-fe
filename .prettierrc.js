bf5d35eeb14fa82b9ed555b13a340ca259758e1f
Andela Rwanda <andelarwanda@Andelas-MacBook-Pro-2.local>
2023-06-07 16:14:02 +0200

    [chore:give me what to include in that file provided that i am only give this ESLint is the required linter for the project. It'll help check for style violations and flag them. The pre-commit hook will run the following commands
    format - which will be set up to use ESlint to format the code base before every commit based on ESlint and Prettier rules
    test - Which will make sure that no code is committed with any test breaking changes
    
    git commit -m'chore . Git Pre commit hook
    -added husky in the package.json
    -created .prettierrc.js file
    -created.eslintrc.cjs file
    ch[50]

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    trailingComma: 'all', // Add trailing commas wherever possible
+    tabWidth: 2, // Set the indentation size to 2 spaces
+    semi: true, // Use semicolons at the end of statements
+    singleQuote: true, // Use single quotes instead of double quotes
+    printWidth: 80, // Limit the line length to 80 characters
+  };
+  
